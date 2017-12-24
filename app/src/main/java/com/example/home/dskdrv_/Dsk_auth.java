879e8e79d9439140c98b5ce94931519c704acc53
A.Zelinsky <zelen23@gmail.com>
2017-12-24 13:30:30 +0300

    default

--- app/src/main/java/com/example/home/dskdrv_/Dsk_auth.java
+++ app/src/main/java/com/example/home/dskdrv_/Dsk_auth.java
@@ -2,6 +2,7 @@ package com.example.home.dskdrv_;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Dsk_auth extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Dsk_auth extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dsk_auth);
+
+        Log.i("push","1commit");
     }
 }
